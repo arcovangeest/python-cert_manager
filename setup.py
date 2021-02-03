2b8757b91bb7140b80de7cf7b1a2442523012972
Arco Geest <d.a.c.vangeest@tudelft.nl>
2021-02-03 14:09:16 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='cert_manager',
+    version='2.0.0',
+    packages=['cert_manager',],
+    license='see LICENSE.txt',
+    long_description=open('README.txt').read(),
+    url='https://github.com/arcovangeest/python-cert_manager'
+)
